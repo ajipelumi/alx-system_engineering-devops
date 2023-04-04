1d720cd8ac348c0832ce29135a01bbdaf1c929e1
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-04 19:17:39 +0100

    puppet header

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -21,8 +21,8 @@ file { '/etc/nginx/sites-enabled/default':
 		index index.html index.htm index.nginx-debian.html;
 		server_name _;
 		location / {
-				add_header X-Served-By $hostname;
-				try_files $uri $uri/ =404;
+				add_header X-Served-By \$hostname;
+				try_files \$uri \$uri/ =404;
 		}
 		rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
 	}',
