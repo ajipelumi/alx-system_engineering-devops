64586cef657b8aad0c57ca6208d6b2ffbe693b6a
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-04-04 23:43:35 +0100

    p

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,20 +1,16 @@
 # This manifest installs and configure an Nginx server.
 
-# Install stdlib for file_line
-class { 'stdlib': }
-
 # Install Nginx
 package { 'nginx':
   ensure   => 'installed',
-  provider => 'apt',
 }
 
 # Add header
 file_line { 'add_header':
   path    => '/etc/nginx/sites-enabled/default',
   line    => "\tadd_header X-Served-By ${hostname};",
-  after   => '^	listen 80 default_server;$',
-  require => Class['stdlib'],
+  match   => '^    listen 80 default_server;$',
+  require => Package['nginx'],
 }
 
 # Start server
