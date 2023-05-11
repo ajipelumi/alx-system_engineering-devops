7e59c44a9c7460dd4b81c8cb011d36db85c71bda
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-05-11 12:45:39 +0100

    puppet fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
 # This puppet file fixes the server.
 exec { 'settings_file':
-    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php"
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    path    => '/usr/bin:/bin'
 }
