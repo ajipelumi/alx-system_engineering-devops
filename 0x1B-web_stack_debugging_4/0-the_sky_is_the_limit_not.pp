34286cf3c80c644b3589715146e3ae10b9bcc94c
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-05-19 19:40:45 +0100

    ulimit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,11 +3,11 @@
 # Edit /etc/default/nginx file
 exec { 'Increase Limit':
     command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 4096\"/' /etc/default/nginx",
-    path    => '/usr/bin:/bin'
+    path    => ['/usr/local/bin/', '/bin/'],
 }
 
 # Restart Nginx
 exec { 'Restart Nginx':
-    command => 'service nginx restart',
-    path    => '/usr/sbin:/sbin'
+    command => 'nginx restart',
+    path    => ['/etc/init.d/'],
 }
