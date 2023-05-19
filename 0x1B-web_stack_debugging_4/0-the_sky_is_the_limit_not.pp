ea0299e90837486071d31ab897ea46db65fef2d6
ajipelumi <ajisafeoluwapelumi@gmail.com>
2023-05-19 19:26:59 +0100

    increase limit

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# This script increases Nginx limit to handle more requests
+
+# Edit /etc/default/nginx file
+exec { 'Increase Limit':
+    command => "sed -i 's/15/4096/g' /etc/default/nginx",
+    path    => '/usr/bin:/bin'
+}
+
+# Restart Nginx
+exec { 'Restart Nginx':
+    command => 'service nginx restart'
+}
